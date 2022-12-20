3a87100f4afbf782a05c9fc1db18fb5b9770cade
LG <LG@Hyunwoo>
2022-12-20 23:05:58 +0900

    12.20 자바복습 완료!

---
+++ java-study/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,17 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		// 컴파일 단계 연산 수행
+		byte result1 = 10 + 20;
+		System.out.println(result1);
+		
+		// byte변수가 피연산자로 사용된 경우
+		byte x = 10;
+		byte y = 20;
+		int result2 = x + y;
+		System.out.println(result2);
+	}
+
+}
