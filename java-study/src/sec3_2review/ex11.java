a64e2b15a800ce9ddbb4ecb04484aac0e7e838c3
LG <LG@Hyunwoo>
2022-12-23 19:45:23 +0900

    11번 문제

--- java-study/src/sec3_2review/ex11.java
+++ java-study/src/sec3_2review/ex11.java
@@ -11,14 +11,18 @@ public class ex11 {
 		System.out.print("아이디:");
 		String name = sc.nextLine();
 		
-		System.out.println("패스워드:");
+		System.out.print("패스워드:");
 		String strPassword = sc.nextLine();
 		int password = Integer.parseInt(strPassword);
 		
 		if(name.equals("java")) {
 			if(password == 12345) {
-				
+				System.out.println("로그인 성공");
+			}else {
+				System.out.println("로그인 실패:패스워드가 틀림");
 			}
+		}else {
+			System.out.println("로그인 실패:아이디 존재하지 않음");
 		}
 	}
 
