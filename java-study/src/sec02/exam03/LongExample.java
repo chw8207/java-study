9111373338f970d7c7748bb904535ac756e2fa3a
LG <chw8207@naver.com>
2022-12-25 11:28:21 +0900

    long타입 커밋!!

--- java-study/src/sec02/exam03/LongExample.java
+++ java-study/src/sec02/exam03/LongExample.java
@@ -3,15 +3,15 @@ package sec02.exam03;
 public class LongExample {
 
 	public static void main(String[] args) {
+		// long타입 변수
 		long var1 = 10;
 		long var2 = 20L;
-//		long var3 = 1000000000000;  // int타입의 허용범위 초과하여 에러 발생
+//		long var3 = 1000000000000;  컴파일 에러 발생
 		long var4 = 1000000000000L;
 		
 		System.out.println(var1);
 		System.out.println(var2);
 		System.out.println(var4);
-
 	}
 
 }
