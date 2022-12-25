4e4ed46d2e7ccb124a0768f99f1607622f71c397
LG <chw8207@naver.com>
2022-12-25 11:38:20 +0900

    이스케이프 출력!

--- java-study/src/sec02/exam06/EscapeExample.java
+++ java-study/src/sec02/exam06/EscapeExample.java
@@ -3,6 +3,7 @@ package sec02.exam06;
 public class EscapeExample {
 
 	public static void main(String[] args) {
+		// 이스케이프 문자 출력
 		System.out.println("번호\t이름\t직업");
 		System.out.print("행 단위 출력\n");
 		System.out.print("행 단위 출력\n");
