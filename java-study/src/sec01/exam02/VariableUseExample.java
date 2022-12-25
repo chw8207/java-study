ab16e1f22b6106f694487fa4342e85a23f589925
LG <chw8207@naver.com>
2022-12-25 11:15:33 +0900

    커밋!!

--- java-study/src/sec01/exam02/VariableUseExample.java
+++ java-study/src/sec01/exam02/VariableUseExample.java
@@ -3,12 +3,13 @@ package sec01.exam02;
 public class VariableUseExample {
 
 	public static void main(String[] args) {
+		// 변수 사용
 		int hour = 3;
 		int minute = 5;
-		System.out.println(hour + "시간" + minute + "분");
+		System.out.println(hour + "시간 " + minute + "분");
 		
-		int totalMinute = (hour * 60) + minute;
-        System.out.println("총 " + totalMinute + "분");
+		int totalMinute = (hour*60) + minute;
+		System.out.println("총" + totalMinute + "분");
 	}
 
 }
