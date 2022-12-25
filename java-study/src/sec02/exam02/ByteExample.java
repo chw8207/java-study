feb2c5efe5d7c2eabb0ec096ff5160eee5ae0afc
LG <chw8207@naver.com>
2022-12-25 11:26:03 +0900

    byte타입 커밋!

--- java-study/src/sec02/exam02/ByteExample.java
+++ java-study/src/sec02/exam02/ByteExample.java
@@ -3,19 +3,19 @@ package sec02.exam02;
 public class ByteExample {
 
 	public static void main(String[] args) {
+		// byte타입 변수
 		byte var1 = -128;
 		byte var2 = -30;
 		byte var3 = 0;
 		byte var4 = 30;
-		byte var5 = 127;
-//		byte var6 = 128;  // byte는 -128~127이므로 컴파일 에러
+		byte var5 = 30;
+//		byte var6 = 128; byte타입 범위는 -128~127이므로 컴파일 에러발생
 		
 		System.out.println(var1);
 		System.out.println(var2);
 		System.out.println(var3);
 		System.out.println(var4);
 		System.out.println(var5);
-
 	}
 
 }
