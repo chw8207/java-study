772d7060f9e951142de7a7e3e392bf1fe497aae1
LG <chw8207@naver.com>
2022-12-25 11:18:27 +0900

    커밋3!!

--- java-study/src/sec01/exam03/VariableExchangeExample.java
+++ java-study/src/sec01/exam03/VariableExchangeExample.java
@@ -3,15 +3,18 @@ package sec01.exam03;
 public class VariableExchangeExample {
 
 	public static void main(String[] args) {
+		// 변수값 교환
 		int x = 3;
 		int y = 5;
-		System.out.println("x:" + x + ", y:" + y);
+		System.out.println("x:" + x + "y:" + y);
 		
-		int temp = x; // 변수 x값 3을 변수 temp에 저장
-		x = y; // 변수 y값 5를 변수 x값에 저장
-		y = temp; // 변수 temp값 3을 변수 y에 저장
-		System.out.println("x:" + x + ", y:" + y);
-
+		// 변수 x값 3을 변수 temp에 저장
+		int temp = x;
+		// 변수 y값 5를 변수 x에 저장
+		x = y;
+		// 변수 temp값 3을 변수 y에 저장
+		y = temp;
+		System.out.println("x:" + x + "y:" + y);
 	}
 
 }
