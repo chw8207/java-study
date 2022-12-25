f75c31a15e51133f013445eb95db68eac4dc1da4
LG <chw8207@naver.com>
2022-12-25 12:00:32 +0900

    정수타입의 연산1

--- java-study/src/sec03/exam03/ByteOperatorExample.java
+++ java-study/src/sec03/exam03/ByteOperatorExample.java
@@ -1,13 +1,12 @@
 package sec03.exam03;
 
-public class ByteOperationExample {
+public class ByteOperatorExample {
 
 	public static void main(String[] args) {
-		// 컴파일 단계 연산 수행
+		// 정수 타입의 연산
 		byte result1 = 10 + 20;
 		System.out.println(result1);
 		
-		// byte변수가 피연산자로 사용된 경우
 		byte x = 10;
 		byte y = 20;
 		int result2 = x + y;
