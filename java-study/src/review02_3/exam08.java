db45c9dd92658b3efe232c1bfdccfd42019e98d3
LG <chw8207@naver.com>
2022-12-26 20:23:10 +0900

    02_3 8번!

---
+++ java-study/src/review02_3/exam08.java
@@ -0,0 +1,13 @@
+package review02_3;
+
+public class exam08 {
+
+	public static void main(String[] args) {
+		// 두 실수를 덧셈 연산하고 소수점 이하 버리기
+		double var1 = 3.5;
+		double var2 = 2.7;
+		int result = (int)(var1 + var2);
+		System.out.println(result);
+	}
+
+}
