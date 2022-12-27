d56f0cdcaabf38ce0e5adccc14e5e4a732ca5095
LG <chw8207@naver.com>
2022-12-27 20:24:50 +0900

    키보드에서 입력된 내용 문자열로 받기

---
+++ java-study/src/sec04/exam05/ScannerExample.java
@@ -0,0 +1,22 @@
+package sec04.exam05;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) throws Exception{
+		// 키보드에서 입력된 내용을 문자열로 얻기
+		Scanner sc = new Scanner(System.in);
+		String inputData;
+		
+		while(true) {
+			inputData = sc.nextLine();
+			System.out.println("입력된 문자열: \"" + inputData + "\"");
+			if(inputData.equals("q")) {
+				break;
+			}
+		}
+		System.out.println("종료");
+	}
+
+}
