a7f26561fefb0e25c4e0198f0363daf71ed537ea
chw8207 <chw8207@naver.com>
2022-12-28 22:21:56 +0900

    대입 연산자

---
+++ java-study/src/sec02/exam10/AssingmentOperatorExample.java
@@ -0,0 +1,20 @@
+package sec02.exam10;
+
+public class AssingmentOperatorExample {
+
+	public static void main(String[] args) {
+		// 대입 연산자
+		int result = 0;
+		result += 10;
+		System.out.println("result=" + result);
+		result -= 5;
+		System.out.println("result=" + result);
+		result *= 3;
+		System.out.println("result=" + result);
+		result /= 5;
+		System.out.println("result=" + result);
+		result %= 3;
+		System.out.println("result=" + result);
+	}
+
+}
