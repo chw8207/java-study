f122dcfc55cc0fae0206c7167145e9e045104b5b
chw8207 <chw8207@naver.com>
2023-01-02 17:04:52 +0900

    1~10출력

---
+++ java-study/src/sec02/exam06/WhilePrintFrom1To10Example.java
@@ -0,0 +1,14 @@
+package sec02.exam06;
+
+public class WhilePrintFrom1To10Example {
+
+	public static void main(String[] args) {
+		// 1부터 10까지 출력
+		int i=1;
+		while(i<=10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
